c249e3517dc4d776d4a3aaa74ff13767659defc7
Xhf <x_hf@foxmail.com>
2020-02-22 18:12:23 +0800

    完成剩余其他工作

--- gui/js/data.js
+++ gui/js/data.js
@@ -1,5 +1,23 @@
 const server = "http://localhost:8089/server_war_exploded/";
 console.log("load");
+$(document).ready(function(){
+    $.get(
+        server+"allStop",function (data) {
+            let obj = JSON.parse(data);
+            let a = {};
+            for(let i in obj){
+                a[obj[i]["stop_name"]]=null;
+            }
+            $('input.autocomplete').autocomplete({
+                data: a
+            });
+
+        }
+    );
+
+
+});
+
 $(document).ready(function () {
     $("#commit").click(function () {
         $.get(
@@ -13,11 +31,14 @@ $(document).ready(function () {
                 console.log("getData");
                 let json = JSON.parse(data);
                 $("#detail").empty();
+                if (json["stops"].length<2){
+                    $("#detail").append($("<div class=\"total\"></div>").text("没有路线") );
+                }
                 showStops(json);
                 if(json['time']<0){
                     $("#detail").append($("<div class=\"total\"></div>").text("共花费"+json["price"]) );
                 }else{
-                    $("#detail").append($("<div class=\"toal\"></div>").text("共花费"+json["time"]));
+                    $("#detail").append($("<div class=\"total\"></div>").text("共花费"+json["time"]));
                 }
             }
         )
@@ -46,12 +67,18 @@ function showStops(data) {
         });
         if(x>y){
             stops.reverse();
-            [x,y]=[y,x]
+            x = stops.findIndex(function (stop) {
+                return stop["stop_name"]===path[i];
+            });
+            y = stops.findIndex(function (stop) {
+                return stop["stop_name"]===path[i+1];
+            });
         }
         $("#detail").
-            append( $("<div class=\"black-text\"></div>").text(lines[i]["name"]))
+            append( $("<div class=\"black-text\" style='margin-top:16px'></div>").text(lines[i]["name"]))
             .append($("<span class=\"not-in\"></span>").text(stops.slice(0,x).map(a=>a.stop_name).join("  ")+"  "))
             .append( $("<span class=\"in\"></span>").text(stops.slice(x,y+1).map(a=>a.stop_name).join("  ")+"  "))
             .append( $("<span class=\"not-in\"></span>").text(stops.slice(y+1).map(a=>a.stop_name).join("  ")+"  "))
+
     }
 }
